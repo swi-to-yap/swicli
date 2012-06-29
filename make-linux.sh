f3c65103e921ae3a1b497b9fbc34e78d486236bd
Douglas R. Miles <logicmoo@gmail.com>
2012-06-28 18:15:46 -0700

    Copy extra files into swicli-inst/

--- make-linux.sh
+++ make-linux.sh
@@ -11,3 +11,7 @@ mcs -unsafe -warn:0 Swicli.Library/*.cs -out:Swicli.Library.dll
 cp Swicli.Library.dll swicli-inst/pl/lib/x86_64-linux/
 mv  Swicli.Library.dll swicli-inst/pl/lib/i386-linux/
 
+cp d*.html swicli-inst/
+cp RE*.* swicli-inst/
+cp in*.* swicli-inst/
+
