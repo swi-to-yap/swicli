affce18cd8b7dee94a3c5415c2f5041bae39897e
Douglas R. Miles <logicmoo@gmail.com>
2012-07-01 18:51:26 -0700

    Adding more ObjectCollection methods

--- Swicli.Library/ObjectCollections.cs
+++ Swicli.Library/ObjectCollections.cs
@@ -46,90 +46,99 @@ namespace Swicli.Library
 {
     public partial class PrologClient
     {
-        private static IEnumerable CreateCollectionOfType(CycFort arrayValue, Type arrayType)
+
+        private static Type GetColType(object al, out bool isGeneric, out MethodInfo[] reflectCache, out Type elementType)
         {
-            if (arrayType.IsArray)
-            {
-                return CreateArrayOfType(arrayValue, arrayType);
-            }
-            if (!typeof (IEnumerable).IsAssignableFrom(arrayType))
-            {
-                Warn("Return as collection?", arrayValue);
-            }
-            IEnumerable al = (IEnumerable) MakeDefaultInstance(arrayType);
-            Type elementType = typeof (object);
-            if (arrayType == null || arrayType.IsGenericTypeDefinition) arrayType = al.GetType();
-            bool isGeneric = arrayType.IsGenericType;
+            elementType = typeof(object);
+            var colType = al.GetType();
+            isGeneric = colType.IsGenericType;
             if (isGeneric)
             {
-                elementType = arrayType.GetGenericArguments()[0];
+                elementType = colType.GetGenericArguments()[0];
             }
-            if (arrayType.GetArrayRank() != 1)
-            {
-                Warn("Non rank==1 " + arrayType);
-            }
-            PlTerm[] terms = ToTermArray(arrayValue);
-            int termsLength = terms.Length;
-            MethodInfo[] reflectCache;
+
             lock (reflectCachesForTypes)
             {
-                if (!reflectCachesForTypes.TryGetValue(arrayType, out reflectCache))
+                if (!reflectCachesForTypes.TryGetValue(colType, out reflectCache))
                 {
-                    reflectCache = reflectCachesForTypes[arrayType] = new MethodInfo[6];
+                    reflectCache = reflectCachesForTypes[colType] = new MethodInfo[6];
                 }
             }
+            return colType;
+        }
+
+        private static IEnumerable CreateCollectionOfType(CycFort fromTerm, Type colType)
+        {
+            if (colType.IsArray)
+            {
+                return CreateArrayOfType(fromTerm, colType);
+            }
+            if (!typeof(IEnumerable).IsAssignableFrom(colType))
+            {
+                Error("Return as collection?", fromTerm);
+                return null;
+            }
+            PlTerm[] terms = ToTermArray(fromTerm);
+            int termsLength = terms.Length;
+            IEnumerable al = (IEnumerable)MakeDefaultInstance(colType);
+            if (termsLength == 0) return al;
+
+            bool isGeneric;
+            MethodInfo[] reflectCache;
+            Type elementType;
+            colType = GetColType(al, out isGeneric, out reflectCache, out elementType);
             if (!isGeneric)
             {
                 for (int i = 0; i < termsLength; i++)
                 {
                     PlTerm term = terms[i];
+                    colAddElementFallback(al, CastTerm(term, elementType), reflectCache);
                 }
                 return al;
             }
             MethodInfo gMethod = reflectCache[6];
             if (gMethod == null)
             {
-                gMethod = reflectCache[6] = typeof (PrologClient).GetMethod("cliAddElement", BindingFlagsJustStatic)
-                                                .MakeGenericMethod(new[] {elementType});
+                gMethod = reflectCache[6] = typeof(PrologClient).GetMethod("colAddElement", BindingFlagsJustStatic)
+                                                .MakeGenericMethod(new[] { elementType });
             }
             for (int i = 0; i < termsLength; i++)
             {
                 PlTerm term = terms[i];
-                gMethod.Invoke(null, new object[] {al, term, arrayType, elementType, reflectCache});
+                gMethod.Invoke(null, new [] {al, CastTerm(term, elementType), reflectCache});
             }
             return al;
         }
-
-        public static bool cliAddElement<T>(IEnumerable<T> enumerable, PlTerm term, Type type, Type elementType, MethodInfo[] reflectCache)
+        public static bool cliAddElement(PlTerm enumerable, PlTerm tvalue)
+        {
+            IEnumerable al = (IEnumerable)CastTerm(enumerable, typeof (IEnumerable));
+            bool isGeneric;
+            MethodInfo[] reflectCache;
+            Type elementType;
+            Type colType = GetColType(al, out isGeneric, out reflectCache, out elementType);
+            object value = CastTerm(tvalue, elementType);
+            if (!isGeneric)
+            {
+                colAddElementFallback(al, value, reflectCache);
+            }
+            MethodInfo gMethod = reflectCache[6];
+            if (gMethod == null)
+            {
+                gMethod = reflectCache[6] = typeof(PrologClient).GetMethod("colAddElement", BindingFlagsJustStatic)
+                                                .MakeGenericMethod(new[] { elementType });
+            }
+            gMethod.Invoke(null, new object[] { al, value, reflectCache });
+            return true;
+        }
+        public static bool colAddElement<T>(IEnumerable<T> enumerable, object value, MethodInfo[] reflectCache)
         {
             if (enumerable is ICollection<T>)
             {
-                var al = (ICollection<T>) enumerable;
-                al.Add((T) CastTerm(term, elementType));
+                var al = (ICollection<T>)enumerable;
+                al.Add((T)value);
                 return true;
             }
-            return cliAddElementFallback(enumerable, term, type, elementType, reflectCache);
-        }
-
-        public static bool cliAddElementFallback(IEnumerable enumerable, PlTerm term, Type type, Type elementType, MethodInfo[] reflectCache) {
-            if (enumerable is ICollection)
-            {
-                var al = (ICollection)enumerable;
-                return cliSetElementFallback(al, al.Count, term, type, elementType, reflectCache);
-            }
-            // this should cause an error (thats ok)
-            return cliSetElementFallback(enumerable, -1, term, type, elementType, reflectCache);
-        }
-
-        public static bool cliSetElement<T>(IEnumerable<T> enumerable, int i, PlTerm term, Type type, Type elementType, MethodInfo[] reflectCache)
-        {
-            if (enumerable is IList<T>)
-            {
-                var al = (IList<T>) enumerable;
-                al[i] = (T) CastTerm(term, elementType);
-                return true;
-            }
-            return cliSetElementFallback(enumerable, i, term, type, elementType, reflectCache);
+            return colAddElementFallback(enumerable, value, reflectCache);
         }
 
         /// <summary>
@@ -147,34 +156,91 @@ namespace Swicli.Library
         /// 5 = removeAll
         /// 6 = cliAddElementGeneric
         /// </summary>
-        private static readonly Dictionary<Type,MethodInfo[]> reflectCachesForTypes = new Dictionary<Type, MethodInfo[]>();
 
-        public static bool cliSetElementFallback(IEnumerable enumerable, int i, PlTerm term, Type type, Type elementType, MethodInfo[] reflectCache) {
+        private static readonly Dictionary<Type, MethodInfo[]> reflectCachesForTypes = new Dictionary<Type, MethodInfo[]>();
+
+        public static string[] setItemMethodNames = new[] { "set_Item", "SetValue", "Set" /*, "Insert", "InsertAt"*/};
+        public static string[] countItemMethodNames = new string[] { "Count", "Size", "Length" };
+        public static string[] addItemMethodNames = new string[] { "Add", "Append" };
+
+        public static bool colAddElementFallback(IEnumerable enumerable, object value, MethodInfo[] reflectCache)
+        {
+            if (enumerable is ICollection)
+            {
+                var al = (ICollection) enumerable;
+                return colSetElementFallback(al, new[] {al.Count}, value, reflectCache);
+            }
+            return AppendReflectively(enumerable.GetType(), enumerable, value, reflectCache);
+        }
+
+        public static bool cliSetElement(PlTerm enumerable, PlTerm indexes, PlTerm tvalue)
+        {
+            IEnumerable al = (IEnumerable) CastTerm(enumerable, typeof (IEnumerable));
+            bool isGeneric;
+            MethodInfo[] reflectCache;
+            Type elementType;
+            Type colType = GetColType(al, out isGeneric, out reflectCache, out elementType);
+            object value = CastTerm(tvalue, elementType);
+            int[] idx = (int[]) CastTerm(indexes, typeof (int[]));
+            if (colType.IsArray)
+            {
+                Array array = (Array)al;
+                array.SetValue(value, idx);
+                return true;
+            }
+            if (!isGeneric)
+            {
+                colSetElementFallback(al, idx, value, reflectCache);
+            }
+            MethodInfo gMethod = reflectCache[6];
+            if (gMethod == null)
+            {
+                gMethod = reflectCache[6] = typeof (PrologClient).GetMethod("colSetElement", BindingFlagsJustStatic)
+                                                .MakeGenericMethod(new[] {elementType});
+            }
+            gMethod.Invoke(null, new object[] {al, idx, value, reflectCache});
+            return true;
+        }
+
+        public static bool colSetElement<T>(IEnumerable<T> enumerable, int[] i, T value, MethodInfo[] reflectCache)
+        {
+            if (enumerable is IList<T>)
+            {
+                var al = (IList<T>)enumerable;
+                al[i[0]] = value;
+                return true;
+            }
+            return colSetElementFallback(enumerable, i, value, reflectCache);
+        }
+
+        public static bool colSetElementFallback(IEnumerable enumerable, int[] i, object value, MethodInfo[] reflectCache)
+        {
 
             if (enumerable is Array)
             {
                 var al = enumerable as Array;
-                al.SetValue(CastTerm(term, elementType), i);
+                al.SetValue(value, i);
                 return true;
             }
             if (enumerable is IList)
             {
                 var al = enumerable as IList;
-                al[i] = CastTerm(term, elementType);
+                al[i[0]] = value;
                 return true;
-            }             
+            }
             // do it all via bad reflection
             ParameterInfo[] pt = null;
-
-            if (reflectCache[0] == null) foreach (var mname in new[] { "set_Item", "SetValue", "Set" /*, "Insert", "InsertAt"*/})
-            {
-                MethodInfo mi = type.GetMethod(mname, BindingFlagsInstance);
-                if (mi == null) continue;
-                pt = mi.GetParameters();
-                if (pt.Length != 2) continue;
-                reflectCache[0] = mi;
-                break;
-            }
+            Type type = null;
+            if (reflectCache[0] == null) foreach (var mname in setItemMethodNames)
+                {
+                    type = type ?? enumerable.GetType();
+                    MethodInfo mi = type.GetMethod(mname, BindingFlagsInstance);
+                    if (mi == null) continue;
+                    pt = mi.GetParameters();
+                    if (pt.Length != 2) continue;
+                    reflectCache[0] = mi;
+                    break;
+                }
 
             MethodInfo reflectCache0 = reflectCache[0];
             if (reflectCache0 != null && reflectCache0 != MissingMI)
@@ -187,51 +253,71 @@ namespace Swicli.Library
                 }
                 if (indexIsFirst)
                 {
-                    reflectCache0.Invoke(enumerable, new object[] { i, CastTerm(term, elementType) });
+                    reflectCache0.Invoke(enumerable, new object[] { i, value });
                 }
                 else
                 {
-                    reflectCache0.Invoke(enumerable, new object[] { CastTerm(term, elementType), i });
+                    reflectCache0.Invoke(enumerable, new object[] { value, i });
                 }
                 return true;
             }
 
             // get the Count
-            int size = -1;
-            if (reflectCache[1] == null) foreach (var mname in new string[] { "Count", "Size", "Length" })
-            {
-                MethodInfo mi = type.GetMethod(mname, BindingFlagsInstance);
-                if (mi != null)
+            if (reflectCache[1] == null) foreach (var mname in countItemMethodNames)
                 {
-                    reflectCache[1] = mi;
-                    break;
+
+                    type = type ?? enumerable.GetType();
+                    MethodInfo mi = type.GetMethod(mname, BindingFlagsInstance);
+                    if (mi != null)
+                    {
+                        reflectCache[1] = mi;
+                        break;
+                    }
                 }
-            }
             reflectCache0 = reflectCache[1];
             if (reflectCache0 != null && reflectCache0 != MissingMI)
             {
                 return Error("Cant even find Count on {0}", enumerable);
             }
-            size = (int)reflectCache[1].Invoke(enumerable, ZERO_OBJECTS);
+            int size = (int)reflectCache[1].Invoke(enumerable, ZERO_OBJECTS);
             // append elements
-            if (i != size)
+            if (i[0] != size)
             {
                 return Error("wrong size for element {0} on {1} with count of {2}", i, enumerable, size);
             }
-            if (reflectCache[2] == null) foreach (var mname in new string[] { "Add", "Append" })
+            return AppendReflectively(type, enumerable, value, reflectCache);
+        }
+
+
+        private static bool AppendReflectively(Type type, IEnumerable enumerable, object value, MethodInfo[] reflectCache)
+        {
+            
+            ParameterInfo[] pt;
+            MethodInfo reflectCache0 = reflectCache[2];
+            if (reflectCache0 == null)
             {
-                MethodInfo mi = type.GetMethod(mname, BindingFlagsInstance);
-                if (mi == null) continue;
-                pt = mi.GetParameters();
-                if (pt.Length != 1) continue;
-                reflectCache[2] = mi;
-                break;
+                foreach (var mname in addItemMethodNames)
+                {
+                    type = type ?? enumerable.GetType();
+                    MethodInfo mi = type.GetMethod(mname, BindingFlagsInstance);
+                    if (mi == null) continue;
+                    pt = mi.GetParameters();
+                    if (pt.Length != 1) continue;
+                    reflectCache0 = reflectCache[2] = mi;
+                    break;
+                }               
             }
-            reflectCache0 = reflectCache[2];
-            if (reflectCache0 != null && reflectCache0 != MissingMI)
+            if (reflectCache0 != null)
             {
-                reflectCache0.Invoke(enumerable, new object[] {CastTerm(term, elementType)});
-                return true;
+                if (reflectCache0 != MissingMI)
+                {
+                    reflectCache0.Invoke(enumerable, new object[] { value });
+                    return true;
+                }
+            }
+            else
+            {
+                reflectCache[2] = MissingMI;
             }
             return Error("No append method on {0}", enumerable);
         }
