e565a6eb7998b56cba0aa6697ca9a5addc9a0f5d
root <logicmoo@gmail.com>
2016-12-28 03:28:05 -0800

    1.1.3

--- pack.pl
+++ pack.pl
@@ -6,5 +6,5 @@ author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 home( 'https://github.com/swi-to-yap/swicli' ).
-download( 'https://github.com/logicmoo/swicli/release/*.zip' ).
+download( 'https://github.com/swi-to-yap/swicli/release/*.zip' ).
 provides(swicffi).
